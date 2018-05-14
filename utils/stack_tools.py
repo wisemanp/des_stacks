14d610eb77bd7f37f7968845915be09e21d34472
wisemanp <philip@wisemen.co.uk>
2018-05-14 15:35:52 -0500

    Debugging

--- utils/stack_tools.py
+++ utils/stack_tools.py
@@ -154,7 +154,9 @@ def make_good_frame_list(s,field,band,cuts={'teff':0.2, 'zp':None,'psf':None}):
             except:
                 t_eff = 2
             if not cuts['psf']:
-                cuts['psf']=5
+                psf_cut=5
+            else:
+                psf_cut = cuts['psf']
             if t_eff > cuts['teff'] and len(this_exp[this_exp['PSF_NEA']>psf_cut])<15:
                 this_exp['T_EFF']= t_eff
                 good_frame = good_frame.append(this_exp)

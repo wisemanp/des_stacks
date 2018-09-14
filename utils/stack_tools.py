0b2b5de06714bccb27b2d7a715c2c862f7d4dca8
wisemanp <philip@wisemen.co.uk>
2018-09-14 15:59:19 +0100

    Debugging

--- utils/stack_tools.py
+++ utils/stack_tools.py
@@ -519,7 +519,7 @@ def resample(s,lst,y,chip,cuts,j,logger,stamp_sizex=4100,stamp_sizey=2100):
                     h = fits.getheader(img)
                 except:
                     h = fits.getheader(img)
-                header_name = os.path.join(s.temp_dir,imgroot+'.head')
+                header_name = os.path.join(s.temp_dir,imgroot+'.resamp.head')
                 if os.path.isfile(header_name):
                     os.remove(header_name)
                 h.totextfile(header_name)

be889720afec0d4c3166f78de2edd74e1cc2d4c2
wisemanp <philip@wisemen.co.uk>
2018-11-20 15:11:57 +0000

    Debugging

--- analysis/astro.py
+++ analysis/astro.py
@@ -786,7 +786,7 @@ def match_gals(catcoord,galscoord,cat,gals,dist_thresh = 2):
     logger.info('Matched %s galaxies with redshifts'%len(stack_gals_with_z))
 
     stack_gals_with_z[['z','z_Err','flag','source']]=stack_gal_zs[['z','z_Err','flag','source']].set_index(stack_gals_with_z.index)
-
+    logger.debug(stack_gals_with_z.columns)
     gals.loc[stack_gals_with_z.index]=stack_gals_with_z
-    logger.debug(gals)
+    logger.debug(gals.columns)
     return gals

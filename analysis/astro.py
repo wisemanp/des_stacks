1f6b878607e0a27a14ccc863b6435617bc02db1b
wisemanp <philip@wisemen.co.uk>
2018-12-05 18:12:37 +0000

    Debugging

--- analysis/astro.py
+++ analysis/astro.py
@@ -796,17 +796,12 @@ def match_gals(catcoord,galscoord,cat,gals,dist_thresh = 2):
         gobj= SkyCoord(ra=g['X_WORLD'].values*u.deg,dec = g['Y_WORLD'].values*u.deg)
         idxc,idxgals,d2d,d3d = gobj.search_around_sky(catcoord,4*u.arcsec)
         grcres = cat.iloc[idxc]
-        if len(grcres[grcres['source']=='DES_AAOmega'])>1:
-            for row in grcres[grcres['source']=='DES_AAOmega'].index:
-                if grcres['ID'].loc[row][:10] =='SVA1_COADD':
-                    ins = grcres[['z','z_Err','flag','source']].loc[row].values
-                    stack_gals_with_z.loc[i,['z','z_Err','flag','source']] = ins
-        elif len(grcres[grcres['source']=='DES_AAOmega'])==1:
-            logger.info('1 has source DES_AAOmega')
-            if grcres[grcres['source']=='DES_AAOmega'].iloc[0,'ID'][:10] =='SVA1_COADD':
-                ins = grcres[grcres['source']=='DES_AAOmega'][['z','z_Err','flag','source']].values
-                stack_gals_with_z.loc[i,['z','z_Err','flag','source']] = ins
 
+        for row in grcres[grcres['source']=='DES_AAOmega'].index:
+            if grcres['ID'].loc[row][:10] =='SVA1_COADD':
+                ins = grcres[['z','z_Err','flag','source']].loc[row].values
+                stack_gals_with_z.loc[i,['z','z_Err','flag','source']] = ins
+        
     gals.loc[stack_gals_with_z.index]=stack_gals_with_z
     logger.debug(gals['z'].nonzero())
     return gals

ca34e03436d23332f2fdb9875b751baab2251b12
wisemanp <philip@wisemen.co.uk>
2018-12-05 13:04:58 +0000

    Making multiprocess

--- bin/make_wms.py
+++ bin/make_wms.py
@@ -9,6 +9,29 @@ from des_stacks.utils.gen_tools import get_good_des_chips, get_des_bands
 good_des_chips = get_good_des_chips()
 bands = get_des_bands()
 
+def worker(img):
+    swarp_cmd = [
+    'swarp',
+    '%s'%img,
+    '-COMBINE','N',
+    '-RESAMPLE','Y',
+    '-WEIGHTOUT_NAME','img'.replace('.fits','wgt.fits')
+    ]
+    print ('Doing following: \n %s '%swarp_cmd)
+    p = subprocess.Popen(swarp_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    outs,errs = p.communicate()
+    if os.path.isfile(img.replace('.fits','.resamp.fits')):
+        os.remove(img.replace('.fits','.resamp.fits'))
+
+def multi_fn(lst):
+    pool = pp.ProcessPool(processes=pool_size,
+                                maxtasksperchild=2,
+                                )
+    pool._clear()
+    pool._serve()
+
+    results = pool.map(lst)
+
 def main():
     fields = ['X1','X2','X3','C1','C2','C3','E1','E2','S1','S2']
     mys = ['1','2','3','4','5','none']
@@ -17,18 +40,7 @@ def main():
             f = 'SN-'+f
             for b in bands:
                 list_of_scis = glob.glob(os.path.join('/media/data3/wiseman/des/coadding/5yr_stacks/','MY%s'%my,f,b,'*clipweighted*'))
-                for img in list_of_scis:
-                    swarp_cmd = [
-                    'swarp',
-                    '%s'%img,
-                    '-COMBINE','N',
-                    '-RESAMPLE','Y',
-                    '-WEIGHTOUT_NAME','img'.replace('.fits','wgt.fits')
-                    ]
-                    p = subprocess.Popen(swarp_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                    outs,errs = p.communicate()
-                    if os.path.isfile(img.replace('.fits','.resamp.fits')):
-                        os.remove(img.replace('.fits','.resamp.fits'))
+                multi_fn(list_of_scis)
 
 if __name__=="__main__":
     main()

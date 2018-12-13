d2e8508e21e6820f9287731fbaa046684f30235a
wisemanp <philip@wisemen.co.uk>
2018-12-13 16:45:50 +0000

    Debugging

--- analysis/astro.py
+++ analysis/astro.py
@@ -694,8 +694,10 @@ def cap_sn_lookup(sn_name,wd = 'coadding',savename = 'all_sn_phot.csv',dist_thre
                         logger.info(snspecobs['Z_GAL'].values[i])
                         try:
                             res_df['z'].loc[ind]=snspecobs['Z_GAL'].values[i]
+                            res_df['z_Err'].loc[ind] = -9999.0
                         except:
                             res_df['z'].loc[ind]=snspecobs['Z_GAL']
+                            res_df['z_Err'].loc[ind] = -9999.0
 
         res_df['EDGE_FLAG'] = get_edge_flags(res_df.X_IMAGE.values,res_df.Y_IMAGE.values)
     if not os.path.isdir('/media/data3/wiseman/des/coadding/5yr_stacks/CAP/%s'%sn_name):

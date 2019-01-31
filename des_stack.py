1749bc468327196ed3fa88624ee9d14c8ea74cbf
wisemanp <philip@wisemen.co.uk>
2019-01-31 10:05:10 +0000

    Debugging string to float issue

--- des_stack.py
+++ des_stack.py
@@ -180,7 +180,7 @@ class Stack():
         self.logger.info('******************************************************')
         #does list of good frames exist?
         self.good_frames = {}
-        for chip in self.chips:
+        for chip in [self.chips]:
             if not os.path.isfile(os.path.join(self.list_dir,'good_exps_%s_%s_%s_%s.csv'%(field,band,chip,self.cutstring))):
                 #get the list of good frames
                 self.logger.info('No good frame list with conditions (%s, %s, %s) yet, making a new one with T_eff> %s, ZP < %s. and PSF < %s' %(field,band,self.cutstring,self.t_cut,self.zp_cut,self.psf_cut))

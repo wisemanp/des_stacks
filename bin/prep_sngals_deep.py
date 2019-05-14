bf0aaf61342bb2891d292969f45edd0c8df29c50
wisemanp <philip@wisemen.co.uk>
2019-05-14 16:29:10 +0100

    Debugging negate indexing in pandas

--- bin/prep_sngals_deep.py
+++ bin/prep_sngals_deep.py
@@ -135,7 +135,7 @@ good_match_inds = np.nonzero(d2d.arcsec <0.6)[0]
 good_deep  = deep.iloc[good_match_inds]
 good_sngals = init_good_sngals.iloc[good_match_inds]
 deep['SNGALID'].loc[good_deep.index] = good_sngals['sngalid'].values
-deep['SNGALID'].loc[~good_deep.index] = np.arange(len(deep.loc[~good_deep.index]))+153653
+deep['SNGALID'].loc[~deep.index.isin(good_deep.index)] = np.arange(len(deep.loc[~deep.index.isin(good_deep.index)]))+sngals['sngalid'].max()
 deep['COADD_OBJECTS_ID']=np.NaN
 deep['COADD_OBJECTS_ID'].loc[good_deep.index] = good_sngals['coadd_objects_id'].values
 sncand = pd.read_csv('/media/data3/wiseman/des/coadding/catalogs/sncand_db.csv',index_col=0)

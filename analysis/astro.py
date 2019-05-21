b7e3db2f035a21df6203c7da1974c7f4ccc00dea
wisemanp <philip@wisemen.co.uk>
2019-05-21 19:21:11 +0100

    Updating issue with ozdes redshifts

--- analysis/astro.py
+++ analysis/astro.py
@@ -898,12 +898,20 @@ def match_gals(catcoord,galscoord,cat,gals,dist_thresh = 2):
                 logger.debug('Alert, found an OzDES redshift')
                 logger.debug(g)
                 logger.debug(grcres)
-
-
                 if grcres['ID'].loc[row][:10] =='SVA1_COADD':
+                    canskip = False
                     if grcres['flag'].loc[row] in ['3','4']:
                         ins = grcres[['z','z_Err','flag','source']].loc[row].values
-                        stack_gals_with_z.loc[i,['z','z_Err','flag','source']] = ins
+                        stack_gals_with_z.loc[i,['SPECZ','SPECZ_ERR','SPECZ_FLAG','SPECZ_CATALOG']] = ins
+                    else: canskip = True
+                else:
+                    if canskip ==True:
+                        if grcres['flag'].loc[row] in ['3','4']:
+                            ins = grcres[['z','z_Err','flag','source']].loc[row].values
+                            stack_gals_with_z.loc[i,['SPECZ','SPECZ_ERR','SPECZ_FLAG','SPECZ_CATALOG']] = ins
+
+
+                
             if hereitis:
                 logger.info('I gonna try')
             for row in grcres[grcres['source']!='DES_AAOmega'].index:

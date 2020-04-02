cff43280ef127ed491a6454a1b36626e81625732
wisemanp <philip@wisemen.co.uk>
2020-04-02 17:17:25 +0100

    Renaming redshift error column

--- analysis/astro.py
+++ analysis/astro.py
@@ -590,7 +590,7 @@ def cap_phot_all(y,f,chip,wd='coadding',autocuts = False):
     #logger.info('Found these OzDES targets with z flag 1 or 2: ')
     #logger.info(low_conf_z[['z','source','flag']])
     matched_cat_df['z'].loc[low_conf_z.index] = -9.9900
-    matched_cat_df['z_Err'].loc[low_conf_z.index] = -9999
+    matched_cat_df['ez'].loc[low_conf_z.index] = -9999
 
     matched_cat_df.to_csv(os.path.join(sg.out_dir,'MY%s'%y,f,'CAP',str(chip),'%s_%s_%s_obj_deep.cat'%(sg.my,sg.field,chip)))
     logger.info("Done CAP on %s, MY%s, CCD %s. Saved result to %s "%(f,y,chip,os.path.join(sg.out_dir,'MY%s'%y,f,'CAP',str(chip),'%s_%s_%s_obj_deep.cat'%(sg.my,sg.field,chip))))

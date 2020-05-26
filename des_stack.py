4f9137f16afaf384fbf2d624fc5d2d53275e5b64
wisemanp <philip@wisemen.co.uk>
2020-05-26 15:06:53 +0100

    Chips are strings

--- des_stack.py
+++ des_stack.py
@@ -215,7 +215,7 @@ class Stack():
             self.chips = []
             for c in range(1,63):
                 if c not in [2,31,61]:
-                    self.chips.append(c)
+                    self.chips.append(str(c))
         self.logger.info("******** Preparing to extract and match soures *******")
         if not self.cutstring:
             if cuts['zp'] and cuts['psf'] and not cuts['teff']:

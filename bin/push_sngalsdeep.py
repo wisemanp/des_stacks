9893b2db6e6ede0f4915df77ed41c994a186bd15
wisemanp <philip@wisemen.co.uk>
2020-08-05 13:52:33 +0100

    Actually execute!

--- bin/push_sngalsdeep.py
+++ bin/push_sngalsdeep.py
@@ -84,10 +84,6 @@ for i in tqdm(range(int(start),len(deep))): #len(deep)
        deep['VERSION'].iloc[i], deep['SNID'].iloc[i], deep['GALFLAG'].iloc[i], deep['HOST'].iloc[i],deep['SEPARATION'].iloc[i],
        deep['EDGE_FLAG'].iloc[i], deep['Z_RANK'].iloc[i]))
     #print (int(deep['SNID'].iloc[i]))
-    '''if not int(deep['SNID'].iloc[i]) in new_snids:
-        print(deep['SNID'].iloc[i],'Fuck')
-    else:
-        print ('Inserting: \n %s'%query)
-        cursor.execute(query)
-        print ('Successfully pushed row %s of %s to SNGALS_DEEP'%(i,len(deep)))'''
+    cursor.execute(query)
+        #print ('Successfully pushed row %s of %s to SNGALS_DEEP'%(i,len(deep)))
 print ('DONE!')
